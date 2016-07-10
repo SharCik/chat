66b682aa264d0e1a7264405c1b7a8f4eaa47fc98
Stas Zelenko <sharcik14@gmail.com>
2016-07-10 14:25:06 +0300

    postgress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name: "Chatty Bot", email: "chattybot@example.com", password: "12345", password_confirmation: "12345")
+User.create!(name: "Admin", email: "admin@mail.ru", password: "12345678", password_confirmation: "12345678", admin: true)
